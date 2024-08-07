8a3a31bf6671a880484d9cbda078ed41d4e741cc
Eleazar Orono <braincorelabdev12@gmail.com>
2024-08-06 20:49:52 -0400

    migration restaurant created

--- database/migrations/2024_08_07_000041_create_restaurant_table.php
+++ database/migrations/2024_08_07_000041_create_restaurant_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('restaurant', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('description', "MAX");
+            $table->string('schedule');
+            $table->string('logo', "MAX");
+            $table->string('long');
+            $table->string('lat');
         });
     }
 
