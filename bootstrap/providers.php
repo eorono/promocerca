4e5764a4f203e86ee08195effd937b56808d3d6a
Eleazar Orono <braincorelabdev12@gmail.com>
2024-08-07 15:50:37 -0400

    Promo table created

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Turso\Driver\Laravel\LibSQLServiceProvider::class,
+
 ];
--- database/migrations/2024_08_07_000052_create_promo_table.php
+++ database/migrations/2024_08_07_000052_create_promo_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('promo', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->bigInteger("restaurant_id")->unsigned();
+            $table->string('name');
+            $table->string('description');
+            $table->string('schedule');
+            $table->foreign('restaurant_id')->references('id')->on('restaurant');
+
         });
     }
 
